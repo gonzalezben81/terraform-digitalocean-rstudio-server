7f81c5656524a3932da714100287e19a36f4d046
gonzalezben81 <gonzalezben393@gmail.com>
2025-11-09 23:43:32 +0000

    Updated versions.tf

--- modules/rstudio-server/versions.tf
+++ modules/rstudio-server/versions.tf
@@ -2,7 +2,9 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
+      version = "~> 2.68.0"
     }
   }
+
+  required_version = ">= 1.5.0"
 }
