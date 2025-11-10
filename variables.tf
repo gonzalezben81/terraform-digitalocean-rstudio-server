03ac93fd653182540e36cec0576e27d25a15df11
gonzalezben81 <gonzalezben393@gmail.com>
2025-11-10 00:01:38 +0000

    Updated modules to just root module to troubleshoot provider issue.

--- variables.tf
+++ variables.tf
@@ -1,38 +1,27 @@
-variable "do_token" {
-  description = "DigitalOcean API token"
-  type        = string
-  sensitive   = true
-}
-
-variable "rstudio_ssh_key_name" {
-  description = "Name of the SSH key registered in DigitalOcean"
-  type        = string
-}
-
 variable "droplet_name" {
-  description = "Name of the Plex droplet"
-  type        = string
-  default     = "rstudio-server"
+  description = ""
+  default     = "rstudio-test"
 }
-
 variable "droplet_region" {
-  description = "DigitalOcean region for deployment"
-  type        = string
+  description = ""
   default     = "nyc3"
 }
 
 variable "droplet_size" {
-  description = "Droplet size slug (e.g., s-1vcpu-1gb)"
-  type        = string
-  default     = "s-1vcpu-1gb"
+  description = ""
+  default     = "s-2vcpu-4gb"
 }
-
 variable "droplet_image" {
-  description = "Droplet image slug (e.g., ubuntu-22-04-x64)"
-  type        = string
-  default     = "ubuntu-22-04-x64"
+  description = ""
+  default     = "ubuntu-24-04-x64"
 }
-
+variable "rstudio_ssh_key_name" {
+  description = ""
+  default     = ""
+}
+variable "do_token" {
+  description = "" # Leave empty to prompt for Digital Ocean API Token
+}
 variable "user_name" {
   description = "Username for droplet"
   default     = "datascience"
